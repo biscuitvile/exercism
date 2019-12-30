50d081efb5c33e2742c86e6f1ed4bcd78c7c65f5
Andy Borsz <andy.borsz@gmail.com>
2019-12-30 13:15:45 -0500

    Add ruby raindrops exercise

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,13 @@
+module Raindrops
+
+  def self.convert(number)
+    output = ""
+
+    output += "Pling" if number % 3 == 0
+    output += "Plang" if number % 5 == 0
+    output += "Plong" if number % 7 == 0
+
+    output == "" ? number.to_s : output
+  end
+
+end
