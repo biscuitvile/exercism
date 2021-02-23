7f7f45010dd3aae8b217ca2bb31b3392628f213c
Andy Borsz <andy.borsz@gmail.com>
2021-02-23 08:39:26 -0500

    Delete ruby and typescript to start over

--- ruby/isogram/isogram.rb
+++
@@ -1,8 +0,0 @@
-module Isogram
-  def self.isogram?(input)
-    input.downcase!
-    input.gsub!(/[ -]/, "")
-
-    input.split("").uniq.count == input.split("").count
-  end
-end
--- typescript/leap/leap.ts
+++
@@ -1,9 +0,0 @@
-function isLeapYear(year: number): boolean {
-  const divisibleBy4: boolean = year % 4 == 0;
-  const divisibleBy100: boolean = year % 100 == 0;
-  const divisibleBy400: boolean = year % 400 == 0;
-
-  return divisibleBy4 && !divisibleBy100 || divisibleBy400;
-}
-
-export default isLeapYear
